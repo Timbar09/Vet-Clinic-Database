79144f7136b1b498529bf3e69286f8cc4805bede
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-03-27 17:19:30 +0200

    Insert animal data

--- schema.sql
+++ schema.sql
@@ -4,8 +4,8 @@ CREATE TABLE animals (
     id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100) NOT NULL,
     date_of_birth DATE,
-    escape_attempts INT DEFAULT 0,
-    neutered BOOLEAN DEFAULT FALSE,
+    escape_attempts INT NOT NULL,
+    neutered BOOLEAN NOT NULL,
     weight_kg DECIMAL,
     PRIMARY KEY (id)
 );
