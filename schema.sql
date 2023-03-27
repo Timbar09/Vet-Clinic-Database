ea8d423b74a289ac4d6e64ccbd63060044ba4bf5
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-03-27 16:58:53 +0200

    create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
-/* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100) NOT NULL,
+    date_of_birth DATE,
+    escape_attempts INT DEFAULT 0,
+    neutered BOOLEAN DEFAULT FALSE,
+    weight_kg DECIMAL,
+    PRIMARY KEY (id)
 );
