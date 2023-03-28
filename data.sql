d3c9e59424de8dfb1fcde06eecf0a2c6c215854d
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-03-28 16:12:40 +0200

    Insert data into owners table

--- data.sql
+++ data.sql
@@ -43,3 +43,24 @@ VALUES ('Blossom', '1998-10-13', 3, TRUE, 17);
 -- INSERT Ditto
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Ditto', '2022-05-14', 4, TRUE, 22);
+
+
+-- Insert data into the owners table
+
+-- Sam Smith
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
+
+-- Jennifer Orwell
+INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
+
+-- Bob
+INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
+
+-- Melody Pond
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
+
+-- Dean Winchester
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
+
+-- Jodie Whittaker
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
