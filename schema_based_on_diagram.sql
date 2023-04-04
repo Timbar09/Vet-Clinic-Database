b29e87919be7401025913d0c3f9a4fd945c85ead
fitz95 <fitzgeraldnsong@gmail.com>
2023-04-04 11:13:42 +0100

    add patients, invoice, invoice_items tables and constraints

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,3 +1,33 @@
+-- Create patients table
+CREATE TABLE patients(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    PRIMARY KEY (id),
+);
+-- Create invoices table
+CREATE TABLE invoices(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    total_amount DECIMAL(10,2) NOT NULL,
+    generated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+    payed_at TIMESTAMP(0) WITHOUT TIME ZONE,
+    medical_histories_id INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_invoices_medical_histories FOREIGN KEY (medical_histories_id) REFERENCES medical_histories (id),
+);
+-- Create invoice_items table
+CREATE TABLE invoice_items(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    unit_price DECIMAL(10,2) NOT NULL,
+    quantity INT NOT NULL,
+    total_price DECIMAL(10,2) NOT NULL,
+    invoice_id INT NOT NULL,
+    treatment_id INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_invoice_items_invoices FOREIGN KEY (invoice_id) REFERENCES invoices (id),
+    CONSTRAINT fk_invoice_items_treatments FOREIGN KEY (treatment_id) REFERENCES treatments (id),
+);
+
 -- Create medical_histories table
 
 CREATE TABLE medical_histories(
